cec899953ce6e13587050c0d991c7355617dbcdc
Gabriel amaral <amaralgabriel357@gmail.com>
2026-01-07 18:32:14 -0300

    modelagem da primeira classe e ciração da table

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/model/NinjaModel.java
@@ -0,0 +1,55 @@
+package dev.java10x.CadastroDeNinjas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private Integer idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, Integer idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
