8d1475c3d0115c8c4183720a1d14efb806d176c0
Gabriel amaral <amaralgabriel357@gmail.com>
2026-01-19 20:53:11 -0300

    chore: .gitignore adiciona minhas credencias no banco de dados

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/NinjaController.java
@@ -1,11 +1,15 @@
 package dev.java10x.CadastroDeNinjas.ninjas;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class NinjaController {
-    
+    @GetMapping("/boasvindas")
+    public String boasvindas() {
+        return "Boas vindas";
+    }
 
 }
